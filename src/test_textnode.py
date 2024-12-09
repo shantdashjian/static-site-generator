7b1b031d69f3cf353c8d3b948ffa78a1961e045a
Shant Dashjian <shantgdashjian@gmail.com>
2024-12-09 18:19:04 +0400

    Fix TextNode class and add tests

---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_not_eq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a another text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_text_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.CODE)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, "url")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+    
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node.__repr__(), "TextNode(This is a text node, bold, None)")
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -11,19 +11,17 @@ class TextType(Enum):
 
 
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
-        for prop in self:
-            if self[prop] != other[prop]:
-                return False
-        for prop in other:
-            if other[prop] != self[prop]:
-                return False
-        return True
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
