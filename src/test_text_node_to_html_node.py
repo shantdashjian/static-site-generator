6ce0764dd15f1430af9327803e525e02a572b109
Shant Dashjian <shantgdashjian@gmail.com>
2025-04-04 01:03:36 +0400

    Add text_node_to_html_node

---
+++ src/test_text_node_to_html_node.py
@@ -0,0 +1,44 @@
+import unittest
+
+from textnode import TextNode, TextType, text_node_to_html_node
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
+    def test_italic(self):
+        node = TextNode("This is a italic text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a italic text node")
+
+    def test_code(self):
+        node = TextNode("This is a code node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code node")
+
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://procodingmentor.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node")
+        self.assertEqual(html_node.props_to_html(), ' href="https://procodingmentor.com"')
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://image.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, None)
+        self.assertEqual(
+            html_node.props_to_html(), ' src="https://image.png" alt="This is an image"'
+        )
