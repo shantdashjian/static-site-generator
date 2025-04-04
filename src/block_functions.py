65fe94b80a2935f7b64caef13bf71da56cf4cff7
Shant Dashjian <shantgdashjian@gmail.com>
2025-04-04 20:05:12 +0400

    Add BlockType and block to block type function

--- src/block_functions.py
+++ src/block_functions.py
@@ -1,3 +1,6 @@
+from blocknode import BlockType
+
+
 def markdown_to_blocks(markdown):
     return list(
         filter(
@@ -5,3 +8,50 @@ def markdown_to_blocks(markdown):
             , map(lambda block: block.strip(" \n"), markdown.split("\n\n"))
         )
     )
+
+def block_to_block_type(block):
+    if is_heading(block):
+        return BlockType.HEADING
+    if is_code(block):
+        return BlockType.CODE
+    if is_quote(block):
+        return BlockType.QUOTE
+    if is_unordered_list(block):
+        return BlockType.UNORDERED_LIST
+    if is_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+
+def is_heading(block):
+    return (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    )
+
+def is_code(block):
+    return block.startswith("```") and block.endswith("```")
+
+def is_quote(block):
+    lines = block.split("\n")
+    for line in lines:
+        if not line.startswith("> "):
+            return False
+    return True
+
+def is_unordered_list(block):
+    lines = block.split("\n")
+    for line in lines:
+        if not line.startswith("- "):
+            return False
+    return True
+
+def is_ordered_list(block):
+    lines = block.split("\n")
+    for i in range(len(lines)):
+        if not lines[i].startswith(f"{i + 1}. "):
+            return False
+    return True
--- src/test_block_functions.py
+++ src/test_block_functions.py
@@ -1,6 +1,7 @@
 import unittest
 
-from block_functions import markdown_to_blocks
+from block_functions import block_to_block_type, markdown_to_blocks
+from blocknode import BlockType
 
 
 class TestMarkDownToBlocks(unittest.TestCase):
@@ -25,3 +26,70 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_header(self):
+        md = """
+# This is a header
+    """
+        block = markdown_to_blocks(md)[0]
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_header_2(self):
+        md = """
+## This is a header
+    """
+        block = markdown_to_blocks(md)[0]
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.HEADING, block_type)
+
+    def test_code(self):
+        md = """
+```
+print("Hello")
+```
+    """
+        block = markdown_to_blocks(md)[0]
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.CODE, block_type)
+
+    def test_quote(self):
+        md = """
+> To Be or Not to Be
+> To Be or Not to Be
+> To Be or Not to Be
+    """
+        block = markdown_to_blocks(md)[0]
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.QUOTE, block_type)
+
+    def test_unordered_list(self):
+        md = """
+- To Be or Not to Be
+- To Be or Not to Be
+- To Be or Not to Be
+    """
+        block = markdown_to_blocks(md)[0]
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.UNORDERED_LIST, block_type)
+
+    def test_ordered_list(self):
+        md = """
+1. To Be or Not to Be
+2. To Be or Not to Be
+3. To Be or Not to Be
+    """
+        block = markdown_to_blocks(md)[0]
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.ORDERED_LIST, block_type)
+
+    def test_paragraph(self):
+        md = """
+To Be or Not to Be
+To Be or Not to Be
+To Be or Not to Be
+    """
+        block = markdown_to_blocks(md)[0]
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.PARAGRAPH, block_type)
