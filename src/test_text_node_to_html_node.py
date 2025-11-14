2b688fe3cc0db795f2324098dc956f21a4e64b81
Shant Dashjian <shantgdashjian@gmail.com>
2025-11-14 18:27:21 +0400

    Add text node to html node

---
+++ src/test_text_node_to_html_node.py
@@ -0,0 +1,43 @@
+import unittest
+
+from text_node_to_html_node import text_node_to_html_node
+from textnode import TextNode, TextType
+
+class TestTextNodeToHtmlNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_italic(self):
+        node = TextNode("This is an italic node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is an italic node")
+
+    def test_code(self):
+        node = TextNode("print(5)", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "print(5)")
+
+    def test_link(self):
+        node = TextNode("Google", TextType.LINK, "https://google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "Google")
+        self.assertEqual(html_node.props, {'href': 'https://google.com'})
+
+    def test_image(self):
+        node = TextNode("Sun", TextType.IMAGE, "https://en.wikipedia.org/wiki/Sun#/media/File:The_Sun_in_white_light.jpg")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {'src': 'https://en.wikipedia.org/wiki/Sun#/media/File:The_Sun_in_white_light.jpg', "alt": "Sun"})
--- src/textnode.py
+++ src/textnode.py
@@ -20,5 +20,3 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
-    
-
