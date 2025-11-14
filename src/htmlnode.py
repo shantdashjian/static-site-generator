bb4e1d49835400e54ebdfddebdb7e1da3be8b9b3
Shant Dashjian <shantgdashjian@gmail.com>
2025-11-14 16:33:45 +0400

    Add leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,15 @@ class HTMLNode:
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("no value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
---
+++ src/test_leafnode.py
@@ -0,0 +1,16 @@
+import unittest
+
+from htmlnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_with_props(self):
+        node = LeafNode("p", "Hello, world!", {"class": "main"})
+        self.assertEqual(node.to_html(), '<p class="main">Hello, world!</p>')
