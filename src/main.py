6172f98225729127c79949eee71713015ded9255
Shant Dashjian <shantgdashjian@gmail.com>
2025-11-15 22:45:18 +0400

    Prep for build and deploy from the docs dir

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import sys
 from copy_content import copy_content
-from generate_page import generate_page
 from generate_pages_recursive import generate_pages_recursive
 
 def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
-    copy_content("static", "public")
-    generate_pages_recursive(basepath, "content", "template.html", "public")
+    copy_content("static", "docs")
+    generate_pages_recursive(basepath, "content", "template.html", "docs")
 
 if __name__ == "__main__":
     main()
