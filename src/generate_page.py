673ffd9825c7c942bbc1c42c51ffd5cc4f382a36
Shant Dashjian <shantgdashjian@gmail.com>
2025-11-15 22:34:36 +0400

    Add basepath as an optional command line argument

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,7 +3,7 @@ from extract_title import extract_title
 from markdown_to_html_node import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(f"{from_path}", "r") as f:
         markdown = f.read()
@@ -12,6 +12,9 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     content = markdown_to_html_node(markdown).to_html()
     full_html = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    full_html = full_html.replace('href="/', f'href="{basepath}')
+    full_html = full_html.replace('src="/', f'src="{basepath}')
+    
     dest_dir = "/".join(dest_path.split("/")[:-1])
     if not os.path.isdir(dest_dir):
         os.mkdir(dest_dir)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+import sys
 from copy_content import copy_content
 from generate_page import generate_page
 from generate_pages_recursive import generate_pages_recursive
 
 def main():
+    basepath = sys.args[1] if len(sys.argv) > 1 else "/"
     copy_content("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive(basepath, "content", "template.html", "public")
 
 if __name__ == "__main__":
     main()
