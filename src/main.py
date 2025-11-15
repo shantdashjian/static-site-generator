0734683b831f1fe6c3fc002431e2d96283e2eff9
Shant Dashjian <shantgdashjian@gmail.com>
2025-11-15 22:39:18 +0400

    Add build script and fix bug in argv

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from generate_page import generate_page
 from generate_pages_recursive import generate_pages_recursive
 
 def main():
-    basepath = sys.args[1] if len(sys.argv) > 1 else "/"
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     copy_content("static", "public")
     generate_pages_recursive(basepath, "content", "template.html", "public")
 
